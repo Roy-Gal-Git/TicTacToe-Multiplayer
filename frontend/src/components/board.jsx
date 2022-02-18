5969f9557279798159e36f8259dbb8de6af260ad
Roy-Gal-Git <self.roygal@gmail.com>
2022-02-18 23:21:57 +0200

    Add player display

--- frontend/src/components/board.jsx
+++ frontend/src/components/board.jsx
@@ -1,44 +1,49 @@
-const Board = ({ table, onTurn }) => {
+import React from "react";
+
+const Board = ({ table, onTurn, player }) => {
   return (
-    <div className="container">
-      <div className="col">
-        {table.slice(0, 3).map((player, index) => (
-          <button
-            onClick={onTurn}
-            key={index}
-            id={index}
-            style={{ height: "100px", width: "100px" }}
-            className="btn btn-danger m-2"
-          >
-            {player}
-          </button>
-        ))}
-      </div>
-      <div className="col">
-        {table.slice(3, 6).map((player, index) => (
-          <button
-            onClick={onTurn}
-            key={index + 3}
-            id={index + 3}
-            style={{ height: "100px", width: "100px" }}
-            className="btn btn-danger m-2"
-          >
-            {player}
-          </button>
-        ))}
-      </div>
-      <div className="col">
-        {table.slice(6, 9).map((player, index) => (
-          <button
-            onClick={onTurn}
-            key={index + 6}
-            id={index + 6}
-            style={{ height: "100px", width: "100px" }}
-            className="btn btn-danger m-2"
-          >
-            {player}
-          </button>
-        ))}
+    <div>
+      <h3>Player {player}</h3>
+      <div className="container">
+        <div className="col">
+          {table.slice(0, 3).map((player, index) => (
+            <button
+              onClick={onTurn}
+              key={index}
+              id={index}
+              style={{ height: "100px", width: "100px" }}
+              className="btn btn-danger m-2"
+            >
+              {player}
+            </button>
+          ))}
+        </div>
+        <div className="col">
+          {table.slice(3, 6).map((player, index) => (
+            <button
+              onClick={onTurn}
+              key={index + 3}
+              id={index + 3}
+              style={{ height: "100px", width: "100px" }}
+              className="btn btn-danger m-2"
+            >
+              {player}
+            </button>
+          ))}
+        </div>
+        <div className="col">
+          {table.slice(6, 9).map((player, index) => (
+            <button
+              onClick={onTurn}
+              key={index + 6}
+              id={index + 6}
+              style={{ height: "100px", width: "100px" }}
+              className="btn btn-danger m-2"
+            >
+              {player}
+            </button>
+          ))}
+        </div>
       </div>
     </div>
   );
