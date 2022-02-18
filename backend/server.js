69a5d7b982fae7eefd24cde947a87e0509c188a3
Roy-Gal-Git <self.roygal@gmail.com>
2022-02-18 23:58:03 +0200

    Add documentation

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,7 @@ io.on("connection", (socket) => {
   connections.push(socket);
 
   try {
+    // Preventing new connections if 2 players are already playing the game
     if (io.engine.clientsCount > connectionLimit) {
       const index = connections.indexOf(socket);
       if (index !== -1) {
@@ -37,12 +38,16 @@ io.on("connection", (socket) => {
       console.log(socket.id, "Disconnected...");
     }
 
+    // If there is only one player connected, show a waiting message
     if (io.engine.clientsCount === 1) {
       connections[0].emit("err", {
         error: true,
         message: "Waiting for another player to join...",
       });
-    } else if (io.engine.clientsCount === 2) {
+    }
+    // If 2 players are connected, reset their game boards
+    // and set the first player to X and the second to O
+    else if (io.engine.clientsCount === 2) {
       handlePlayer(connections[0], [...emptyTable], "X");
       handlePlayer(connections[1], [...emptyTable], "O");
     }
@@ -50,10 +55,13 @@ io.on("connection", (socket) => {
     console.log(err.message);
   }
 
+  // echo the last play
   socket.on("turn", (payload) => {
     io.emit("turn", payload);
   });
 
+  // handle disconnection of a client by removing his socket from the connections array
+  // and sending the remaining player a waiting message
   socket.on("disconnect", (payload) => {
     const index = connections.indexOf(socket);
     if (index !== -1) {
